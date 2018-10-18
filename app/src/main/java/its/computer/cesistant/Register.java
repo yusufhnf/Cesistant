16ef5b56cd36002454955a00d1cc8da8391cba2d
SUPERBOX\Yusuf Umar Hanafi <flyme.yusuf@gmail.com>
2018-10-18 11:54:30 +0700

    +ambil gambar +menampilkan gambar

--- app/src/main/java/its/computer/cesistant/Register.java
+++ app/src/main/java/its/computer/cesistant/Register.java
@@ -1,15 +1,24 @@
 package its.computer.cesistant;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.AnimationDrawable;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class Register extends AppCompatActivity {
     RelativeLayout container;
     AnimationDrawable anim;
+    ImageView viewimage;
+
+    private static final int CAMERA_REQUEST = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +27,18 @@ public class Register extends AppCompatActivity {
 
         container = (RelativeLayout) findViewById(R.id.containerr);
 
+        viewimage = (ImageView) findViewById(R.id.view_image);
+
         anim = (AnimationDrawable) container.getBackground();
         anim.setEnterFadeDuration(6000);
         anim.setExitFadeDuration(2000);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            viewimage.setImageBitmap(photo);
+        }
     }
 
     @Override
@@ -41,5 +59,7 @@ public class Register extends AppCompatActivity {
     }
 
     public void btn_foto(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST);
     }
 }
